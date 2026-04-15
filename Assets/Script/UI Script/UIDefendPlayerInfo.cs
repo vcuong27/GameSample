d970edff2381078e47360e7339fb680465814d54
NguyenVanCuong <cuong.ktpm@gmail.com>
2026-04-15 23:06:24 +0700

    + Add UI

---
+++ Assets/Script/UI Script/UIDefendPlayerInfo.cs
@@ -0,0 +1,21 @@
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UIDefendPlayerInfo : MonoBehaviour
+{
+    [SerializeField]
+    private Image PlayerImg;
+    [SerializeField]
+    private TMPro.TextMeshProUGUI Level;
+    [SerializeField]
+    private TMPro.TextMeshProUGUI Building;
+    [SerializeField]
+    private TMPro.TextMeshProUGUI Coin;
+
+
+    public void Init()
+    {
+
+    }    
+
+}
--- Assets/Script/UI Script/UIMainBattle.cs
+++ Assets/Script/UI Script/UIMainBattle.cs
@@ -1,9 +1,26 @@
 using System;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class UIMainBattle : MonoBehaviour
 {
-   
+    [SerializeField]
+    private UIDefendPlayerInfo DefendPlayerInfo;
+
+    [SerializeField]
+    private UIBatterInfor BatterInfo;
+
+    public void Init()
+    {
+        DefendPlayerInfo.Init();
+        //BatterInfo.Init();
+    }
+
+    public void UPdateBattleInfor()
+    {
+        //BatterInfo.Update();
+    }    
+
     public void ShowPrepareScreen()
     {
 
